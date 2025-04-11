ebedbbbe4c4c04d407643fcbe9ac6facfd70c324
Y-Wisdom <tnsnames.ora.nis@gmail.com>
2025-04-11 22:13:45 +0900

    fix:sh

--- Discord_webhook/start.sh
+++ Discord_webhook/start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "Webhook server starting..."
-python3 webhook_server.py &
 
-inotifywait -m -e modify webhook_server.py |
+SCRIPT_PATH="/app/webhook_server.py"
+
+python3 "$SCRIPT_PATH" &
+
+inotifywait -m -e modify "$SCRIPT_PATH" |
 while read path action file; do
     echo "File ${file} changed, restarting server..."
-    pkill -f webhook_server.py
-    python3 webhook_server.py &
+    pkill -f "$SCRIPT_PATH"
+    python3 "$SCRIPT_PATH" &
 done
